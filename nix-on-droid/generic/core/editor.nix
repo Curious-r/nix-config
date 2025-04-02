a656400186576fe28ab32b1f32086f7b60bb6f78
Curious <Curious@curious.host>
2025-04-02 22:57:25 +0800

    pkgs.helix is more suitable for nix-on-droid

--- nix-on-droid/generic/core/editor.nix
+++ nix-on-droid/generic/core/editor.nix
@@ -1,10 +1,9 @@
 {
   pkgs,
-  inputs,
   ...
 }:
 {
   environment.packages = with pkgs; [
-    inputs.helix.packages."${pkgs.system}".helix # or some other editor, e.g. nano or neovim
+    helix # or some other editor, e.g. nano or neovim
   ];
 }
