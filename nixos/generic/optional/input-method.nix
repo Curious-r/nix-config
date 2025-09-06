fe9904949c00bc96f1b28fca91557f630c229ff9
Curious <Curious@curious.host>
2025-09-06 13:00:04 +0800

    fix fcitx-rime

--- nixos/generic/optional/input-method.nix
+++ nixos/generic/optional/input-method.nix
@@ -1,8 +1,19 @@
-{ pkgs, self, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  self,
+  ...
+}:
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+    package = lib.mkForce (
+      pkgs.unstable.qt6Packages.fcitx5-with-addons.override {
+        inherit (config.i18n.inputMethod.fcitx5) addons;
+      }
+    );
     fcitx5.addons = with pkgs; [
       fcitx5-gtk # alternatively, kdePackages.fcitx5-qt
       fcitx5-material-color # a color theme
