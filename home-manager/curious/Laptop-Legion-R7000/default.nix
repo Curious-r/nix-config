39167575ed608b8f501a2ff58cbee3f0a1b233f8
Curious <Curious@curious.host>
2025-09-13 10:42:16 +0800

    Temporarily disable rclone

--- home-manager/curious/Laptop-Legion-R7000/default.nix
+++ home-manager/curious/Laptop-Legion-R7000/default.nix
@@ -9,7 +9,7 @@
     ../generic/optional/browser/zen.nix
     ../generic/optional/thunderbird.nix
     ../generic/optional/openscad.nix
-    ../generic/optional/rclone.nix
+    # ../generic/optional/rclone.nix
   ];
   home.stateVersion = "25.05";
 }
