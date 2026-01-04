605943c7b78f414e539c7624134d6f379533b2da
Curious <Curious@curious.host>
2026-01-04 14:32:17 +0800

    R7000 change to use latest kernel

--- nixos/Laptop-Legion-R7000/boot.nix
+++ nixos/Laptop-Legion-R7000/boot.nix
@@ -2,7 +2,6 @@
 {
   # Use the systemd-boot EFI boot loader.
   boot = {
-    kernelPackages = pkgs.linuxPackages;
     loader = {
       systemd-boot.enable = true;
       systemd-boot.consoleMode = "auto";
