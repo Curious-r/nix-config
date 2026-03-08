c33ecb389c14a8a28119de9fa450409dafaaf6ea
Curious <Curious@curious.host>
2026-03-09 00:24:02 +0800

    chore: ghostty config update

--- home-manager/curious/common/optional/terminal/ghostty.nix
+++ home-manager/curious/common/optional/terminal/ghostty.nix
@@ -7,6 +7,7 @@
       font-size = 12;
       font-family = "Monaspace Neon Var";
       font-feature = "calt, liga, ss01, ss02, ss03, ss04, ss05, ss06, ss07, ss08, ss09, ss10";
+      shell-integration-features = "cursor, sudo, title,  ssh-terminfo, ssh-env";
     };
     systemd.enable = true;
     enableBashIntegration = true;
