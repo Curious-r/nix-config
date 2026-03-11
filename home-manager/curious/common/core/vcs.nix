5b98389f8a83a12faac690a97c9069a3d74aec83
Curious <Curious@curious.host>
2026-03-11 18:55:20 +0800

    chore: remove git in hm

--- home-manager/curious/common/core/vcs.nix
+++ home-manager/curious/common/core/vcs.nix
@@ -1,15 +1,4 @@
 { pkgs, ... }:
 {
-  programs = {
-    git = {
-      enable = true;
-      settings = {
-        user = {
-          name = "Curious";
-          email = "Curious@curious.host";
-        };
-      };
-    };
-  };
   home.packages = with pkgs; [ pijul ];
 }
