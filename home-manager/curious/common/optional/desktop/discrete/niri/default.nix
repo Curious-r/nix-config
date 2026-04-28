ac4505a36810fd3d01939f9caa6e2f594532b7fe
Curious <Curious@curious.host>
2026-04-28 20:08:15 +0800

    fix(env): temporary add TERMINAL env for cli app with .desktop

--- home-manager/curious/common/optional/desktop/discrete/niri/default.nix
+++ home-manager/curious/common/optional/desktop/discrete/niri/default.nix
@@ -4,4 +4,11 @@
   home.packages = with pkgs; [
     xwayland-satellite
   ];
+
+  # 有些命令行程序的 .desktop 文件中有 Terminal=true 的配置，DMS 的启动器会使用 $TERMINAL 环境变量来唤起终端
+  # 上游有这个 issue，不直到后面会不会有什么别的方法解决
+  # 既然我们在 niri 快捷键里都写死 Mod + T 唤起 ghostty 了，那我们暂时硬编码这个环境变量的配置也写在这里吧
+  systemd.user.sessionVariables = {
+    TERMINAL = "ghostty";
+  };
 }
