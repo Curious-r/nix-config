df620450968ea992af0d910dbeebf8bc8286d9eb
Curious <Curious@curious.host>
2026-05-12 03:14:57 +0800

    fix(security): sudo & pam & polkit config update

--- nixos/common/core/security.nix
+++ nixos/common/core/security.nix
@@ -1,12 +1,13 @@
-{ ... }:
+{ lib, ... }:
 {
-  security.sudo-rs = {
-    enable = true;
-    wheelNeedsPassword = false;
+  security = {
+    pam.sshAgentAuth.enable = true;
+    sudo-rs = {
+      enable = true;
+      extraConfig = ''
+        Defaults timestamp_timeout=120
+      '';
+    };
+    polkit.enable = lib.mkDefault true;
   };
-  security.sudo.extraConfig = ''
-    Defaults timestamp_timeout=120 # only ask for password every 2h
-    # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
-    # Defaults env_keep + =SSH_AUTH_SOCK
-  '';
 }
