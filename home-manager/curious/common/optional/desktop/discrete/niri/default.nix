37a3cd657d177c51f30c23dbcba86e8d9e645059
Curious <Curious@curious.host>
2026-05-18 02:38:06 +0800

    chore(niri): add animations

--- home-manager/curious/common/optional/desktop/discrete/niri/default.nix
+++ home-manager/curious/common/optional/desktop/discrete/niri/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  xdg.configFile."niri/animations".source = ./animations;
   home.packages = with pkgs; [
     xwayland-satellite
   ];
