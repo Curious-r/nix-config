59d5290b3db8a284b8e97ad6f2da9e3a72aa2288
Curious <Curious@curious.host>
2026-05-17 13:20:33 +0800

    fix(package): fix aerion oauth

--- pkgs/aerion.nix
+++ pkgs/aerion.nix
@@ -1,8 +1,10 @@
 {
   lib,
+  stdenv,
   buildGoModule,
   buildNpmPackage,
   fetchFromGitHub,
+  fetchurl,
   pkg-config,
   copyDesktopItems,
   wrapGAppsHook3,
@@ -15,11 +17,29 @@ let
   version = "0.2.3";
   commitRev = "95a57a27bf58be762e82ca25f66048d1d4504034";
 
+  archMap = {
+    "x86_64-linux" = "x86_64";
+    "aarch64-linux" = "aarch64";
+  };
+  sysArch =
+    archMap.${stdenv.hostPlatform.system}
+      or (throw "Unsupported architecture: ${stdenv.hostPlatform.system}");
+
+  shimHashes = {
+    "x86_64" = "sha256-Wn0hj/L8C69KNlCFHxmVEgTpIuBlmsNewhTXayMIV2s=";
+    "aarch64" = "sha256-4ZxJ1qTJYd2kC5zsHvOzAiST0SgWuezNM+iPrQT0eMY=";
+  };
+
+  credsShim = fetchurl {
+    url = "https://github.com/hkdb/aerion/releases/download/v${version}/flathub-build-env-v${version}-linux-${sysArch}";
+    hash = shimHashes.${sysArch};
+  };
+
   src = fetchFromGitHub {
     owner = "hkdb";
     repo = "aerion";
     rev = commitRev;
-    hash = "sha256-QOYqPnmOfi60AMufuVlASP4iV4YV2KyCTXH57OEC+Kg=";
+    hash = "sha256-Bf3ezUC5Jbqqw3Z0fbWDKBj8hdpRPxuMhVtwF2sD4eQ=";
   };
 
   frontend = buildNpmPackage {
@@ -59,6 +79,10 @@ buildGoModule {
     glib
   ];
 
+  desktopItems = [
+    "build/linux/aerion.desktop"
+  ];
+
   tags = [
     "desktop"
     "production"
@@ -72,14 +96,12 @@ buildGoModule {
 
   postInstall = ''
     mkdir -p $out/share/applications $out/share/pixmaps
-
-    if [ -f build/linux/aerion.desktop ]; then
-      install -Dm644 build/linux/aerion.desktop -t $out/share/applications/
-    fi
-
     if [ -f build/linux/aerion.png ]; then
-      install -Dm644 build/linux/aerion.png $out/share/pixmaps/aerion.png
+      install -Dm644 build/linux/aerion.png $out/share/pixmaps/io.github.hkdb.Aerion.png
     fi
+
+    cp ${credsShim} $out/bin/aerion-creds
+    chmod +x $out/bin/aerion-creds
   '';
 
   meta = with lib; {
@@ -87,5 +109,6 @@ buildGoModule {
     homepage = "https://github.com/hkdb/aerion";
     license = licenses.mit;
     mainProgram = "aerion";
+    maintainers = with maintainers; [ curious ];
   };
 }
