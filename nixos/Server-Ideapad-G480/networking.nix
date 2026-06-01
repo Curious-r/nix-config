1736c58a69646134b7da36652ea6080fcf529036
Curious <Curious@curious.host>
2026-06-01 20:32:09 +0800

    feat(config): use ip token in g480 networking

--- nixos/Server-Ideapad-G480/networking.nix
+++ nixos/Server-Ideapad-G480/networking.nix
@@ -7,6 +7,7 @@
     hostName = "Server-Ideapad-G480"; # Define your hostname.
     timeServers = [ "ntp.aliyun.com" ];
     wireless.iwd.enable = true;
+    useNetworkd = true;
     firewall = {
       enable = true;
       allowedTCPPorts = [
@@ -25,5 +26,19 @@
     #   default = "socks://192.168.1.8:7890";
     #   noProxy = "127.0.0.1,localhost,internal.domain";
     # };
+  };
+  systemd.network = {
+    networks."10-lan" = {
+      matchConfig.Name = "enp2s0";
+
+      networkConfig = {
+        DHCP = "yes";
+        IPv6AcceptRA = true;
+      };
+
+      ipv6AcceptRAConfig = {
+        Token = "::fade:480";
+      };
+    };
   };
 }
