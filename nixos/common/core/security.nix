c8fbae99d6b14e737632017e11b43656551a630c
Curious <Curious@curious.host>
2026-06-14 12:18:17 +0800

    chore(config): remove sudo-rs

--- nixos/common/core/security.nix
+++ nixos/common/core/security.nix
@@ -2,12 +2,6 @@
 {
   security = {
     pam.sshAgentAuth.enable = true;
-    sudo-rs = {
-      enable = true;
-      extraConfig = ''
-        Defaults timestamp_timeout=120
-      '';
-    };
     polkit.enable = lib.mkDefault true;
   };
 }
