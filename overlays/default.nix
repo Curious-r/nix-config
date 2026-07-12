c9cb9ca56f521f937c195963168d6ea4e4c6bef2
Curious <Curious@curious.host>
2026-07-12 11:46:44 +0800

    chore(config): remove the temporary measure for khal build

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,17 +15,6 @@
       # example = prev.example.overrideAttrs (oldAttrs: rec {
       # ...
       # });
-
-      # 临时修复 py 3.14 引起的 khal 构建失败：使用 overrideScope 局部修改 python3Packages 作用域
-      khal = prev.khal.override {
-        python3Packages = prev.python3Packages.overrideScope (
-          pythonFinal: pythonPrev: {
-            click-threading = pythonPrev.click-threading.overridePythonAttrs (old: {
-              doCheck = false;
-            });
-          }
-        );
-      };
     };
   };
 }
