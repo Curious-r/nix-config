5234491ca8dae8045a8fb56c2babe195b0656490
Curious <Curious@curious.host>
2026-07-24 11:41:37 +0800

    chore(packages): disable local aerion since it is merged into nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,15 +2,20 @@
 # You can build them using 'nix build .#example'
 pkgs:
 let
-  aerion-creds = pkgs.callPackage ./aerion-creds.nix { };
+  # aerion-creds = pkgs.callPackage ./aerion-creds.nix { };
 in
 {
   # example = pkgs.callPackage ./example { };
-  inherit aerion-creds;
 
-  # aerion-creds is explicitly passed here (not relying on callPackage to find it in pkgs),
+  # ======================================================================================
+  # Aerion has been merged into nixpkgs, keep it there to be a example for packages with
+  # optianal binary shim
+  # inherit aerion-creds;
+
+  # `aerion-creds` is explicitly passed here (not relying on callPackage to find it in pkgs),
   # because aerion-creds is defined in this same set and may not be available in the pkgs
   # passed to this function — e.g. when called from perSystem (flake-parts) where custom
-  # overlays aren't automatically applied.
-  aerion = pkgs.callPackage ./aerion.nix { inherit aerion-creds; };
+  # overlays aren't automatically applied
+  # aerion = pkgs.callPackage ./aerion.nix { inherit aerion-creds; };
+  # ======================================================================================
 }
