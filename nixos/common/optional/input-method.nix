4356dd4b51bd39d1c4bc95b320a8f5e83d54b13d
Curious <Curious@curious.host>
2026-07-31 22:13:21 +0800

    chore(config): move fcitx5 settings to nixos config

--- nixos/common/optional/input-method.nix
+++ nixos/common/optional/input-method.nix
@@ -1,16 +1,42 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
+
+    fcitx5.waylandFrontend = true;
+
     fcitx5.addons = with pkgs; [
       fcitx5-material-color # a color theme
       (fcitx5-rime.override {
         rimeDataPkgs = [ rime-wanxiang ];
       })
     ];
+
+    fcitx5.settings = {
+      globalOptions = {
+        "Hotkey/AltTriggerKeys" = {
+          "0" = ""; # 将替代触发键置为空字符串，从而禁用 Shift 切换逻辑
+        };
+        # 确保万无一失，把主触发键固定为 Ctrl+Space
+        "Hotkey/TriggerKeys" = {
+          "0" = "Control+space";
+        };
+      };
+
+      # 顺便锁定输入法列表，防止重装后出现英文键盘
+      inputMethod = {
+        "Groups/0" = {
+          Name = "Default";
+          "Default Layout" = "us";
+          DefaultIM = "rime";
+        };
+
+        "Groups/0/Items/0" = {
+          Name = "rime";
+          Layout = "";
+        };
+      };
+    };
   };
 }
