f45b9abb53c65b385c6a8b7cb445340b624d4eb3
Curious <Curious@curious.host>
2026-08-03 12:37:11 +0800

    fix(config): add gcr in dbus packages

--- nixos/common/optional/desktop/discrete/oo7/basic.nix
+++ nixos/common/optional/desktop/discrete/oo7/basic.nix
@@ -6,9 +6,8 @@
   };
 
   # 用于认证弹窗
-  environment.systemPackages = with pkgs; [
-    gcr
-  ];
+  environment.systemPackages = with pkgs; [ gcr ];
+  services.dbus.packages = with pkgs; [ gcr ];
 
   # 开启一些基础的规则钩子，等 #544377 合并之后估计会冲突，到时候删除
   security.pam.services = {
