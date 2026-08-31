f63cfb6e3e971e2bd6c81a35a4570434d9f1339c
Curious <Curious@curious.host>
2026-08-31 21:55:31 +0800

    refactor(ci): clarify configuration job generation

--- ci/jobs.nix
+++ ci/jobs.nix
@@ -5,66 +5,66 @@ let
   };
 
   machines = import ../lib/machines.nix;
+  homeConfigurations = import ../home-manager;
+  nixOnDroidConfigurations = import ../nix-on-droid;
 
-  mkSystemJob =
-    name: machine:
+  # Fallback for Nix versions without builtins.mapAttrsToList (e.g. Lix 2.95)
+  mapAttrsToList =
+    builtins.mapAttrsToList
+      or (f: attrs: builtins.map (name: f name attrs.${name}) (builtins.attrNames attrs));
+
+  mkNixosJob = configName: host: {
+    name = "nixos ${configName} (${host.system})";
+    target = "nixos";
+    machine = configName;
+    system = host.system;
+    entrypoint = "nixos/default.nix";
+    attr = "${configName}.config.system.build.toplevel";
+    prefetch = "";
+    runsOn = runnerFor.${host.system};
+    impure = false;
+  };
+
+  mkHomeManagerJob =
+    configName:
     let
-      system = machine.system;
+      hostName = builtins.elemAt (builtins.split "@" configName) 2;
+      system = machines.${hostName}.system;
     in
     {
-      name = "nixos ${name} (${system})";
-      target = "nixos";
-      machine = name;
+      name = "home-manager ${configName} (${system})";
+      target = "home-manager";
+      machine = configName;
       system = system;
-      entrypoint = "nixos/default.nix";
-      attr = "${name}.config.system.build.toplevel";
+      entrypoint = "home-manager/default.nix";
+      attr = ''"${configName}".activationPackage'';
       prefetch = "";
       runsOn = runnerFor.${system};
       impure = false;
     };
 
-  mkHomeJob = machine: system: {
-    name = "home-manager ${machine} (${system})";
-    target = "home-manager";
-    machine = machine;
-    system = system;
-    entrypoint = "home-manager/default.nix";
-    attr = ''"${machine}".activationPackage'';
-    prefetch = "";
-    runsOn = runnerFor.${system};
-    impure = false;
-  };
-
-  mkDroidJob = machine: prefetch: {
-    name = "nix-on-droid ${machine} (aarch64-linux)";
+  mkNixOnDroidJob = configName: {
+    name = "nix-on-droid ${configName} (aarch64-linux)";
     target = "nix-on-droid";
-    machine = machine;
+    machine = configName;
     system = "aarch64-linux";
     entrypoint = "nix-on-droid/default.nix";
-    attr = ''"${machine}".activationPackage'';
-    prefetch = prefetch;
+    attr = ''"${configName}".activationPackage'';
+    prefetch = "/nix/store/dvf2ck9bkw7yyrlkjk87xz1anaxsgrd6-proot-termux-static-aarch64-unknown-linux-android-unstable-2026-02-20";
     runsOn = runnerFor.aarch64-linux;
     # Upstream embeds an absolute bootstrap store path with
     # builtins.storePath, so evaluation still requires --impure.
     impure = true;
   };
 
-  legacyJobs = [
-    (mkHomeJob "curious@Laptop-Legion-R7000" "x86_64-linux")
-    (mkHomeJob "curious@Router-RaspberryPi-4B-1" "aarch64-linux")
-    (mkHomeJob "curious@Server-IdeaPad-G480" "x86_64-linux")
-  ]
-  ++
-    builtins.map
-      (
-        machine:
-        mkDroidJob machine "/nix/store/dvf2ck9bkw7yyrlkjk87xz1anaxsgrd6-proot-termux-static-aarch64-unknown-linux-android-unstable-2026-02-20"
-      )
-      [
-        "Pad-Vivo-3Pro"
-        "Phone-Redmi-K50Pro"
-      ];
+  nixosJobs = mapAttrsToList (configName: host: mkNixosJob configName host) machines;
+
+  homeManagerJobs = mapAttrsToList (configName: _: mkHomeManagerJob configName) homeConfigurations;
+
+  nixOnDroidJobs = mapAttrsToList (
+    configName: _: mkNixOnDroidJob configName
+  ) nixOnDroidConfigurations;
+
+  manualJobs = [ ];
 in
-builtins.sort (a: b: a.name < b.name) (
-  builtins.map (name: mkSystemJob name machines.${name}) (builtins.attrNames machines) ++ legacyJobs
-)
+builtins.sort (a: b: a.name < b.name) (nixosJobs ++ homeManagerJobs ++ nixOnDroidJobs ++ manualJobs)
