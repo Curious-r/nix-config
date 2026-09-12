33011af10fc61fc43ede1d81d204e43c08e2ec52
Curious <Curious@curious.host>
2026-09-13 03:52:47 +0800

    fix(config): remove outdated options to fix build failures

--- nixos/common/optional/desktop/discrete/dms-shell.nix
+++ nixos/common/optional/desktop/discrete/dms-shell.nix
@@ -14,12 +14,10 @@
     };
 
     # Core features
-    enableSystemMonitoring = true; # System monitoring widgets (dgop)
     enableVPN = true; # VPN management widget
     enableDynamicTheming = true; # Wallpaper-based theming (matugen)
     enableAudioWavelength = true; # Audio visualizer (cava)
     enableCalendarEvents = true; # Calendar integration (khal)
-    enableClipboardPaste = true; # Pasting from the clipboard history (wtype)
 
     plugins = {
       # Simply enable plugins by their ID (from the registry).
