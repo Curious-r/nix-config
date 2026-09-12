20e19eff631d1bdaa2144c0d36433ab2047b7c98
Curious <Curious@curious.host>
2026-09-13 05:33:58 +0800

    chore(config): declare priority for mirror substituters

--- home-manager/curious/common/optional/nix/substituters/mainland.nix
+++ home-manager/curious/common/optional/nix/substituters/mainland.nix
@@ -3,9 +3,9 @@
   nix.settings.substituters = [
     # Cache mirror located in mainland China.
     # Status: https://mirrors.tuna.tsinghua.edu.cn/
-    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=10"
     # Status: https://mirrors.sjtug.sjtu.edu.cn/
-    "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
+    "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store?priority=20"
     # Status: https://mirrors.ustc.edu.cn/status/
     # "https://mirrors.ustc.edu.cn/nix-channels/store"
   ];
