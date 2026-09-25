a35842691c65f7351f72eacb7a89a219dbdcfabf
Curious <Curious@curious.host>
2026-09-25 09:14:48 +0800

    chore(config): daed.package switch to our nix-packages, since nixpkgs is going to drop it

--- nixos/common/optional/daed.nix
+++ nixos/common/optional/daed.nix
@@ -1,8 +1,9 @@
-{ nixosModules, ... }:
+{ nixosModules, pkgs, ... }:
 {
   imports = [ nixosModules.daed ];
   services.daed = {
     enable = true;
+    package = pkgs.curious.daed;
     openFirewall = {
       enable = true;
       port = 12345;
